ccc20e49affa1a7a24add7f15efede14a2fc271e
Pete <petergarvin1@gmail.com>
2019-08-19 19:17:08 +0100

    credit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+// Bret Cameron - https://medium.com/@bretcameron/a-beginners-guide-to-redux-with-react-50309ae09a14
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
